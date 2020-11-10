e207859da7352d2c18e8903e50925c578fdacd46
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2020-11-10 21:54:16 +0100

    Target .NET 5.0

--- src/Internal/HdfFile.cs
+++ src/Internal/HdfFile.cs
@@ -7,9 +7,7 @@
     using System.IO;
     using System.Linq;
     using System.Runtime.InteropServices;
-#if NETSTANDARD2_1
     using System.Text;
-#endif
 
 
     public class HdfFile : IDisposable
@@ -56,121 +54,6 @@
             }, new IntPtr());
         }
 
-#if NETSTANDARD2_0
-        public HdfData<TValue> GetData<TValue>(string datasetPath)
-        {
-            var datasetId = H5D.open(FileIdentifier, datasetPath);
-
-            if (datasetId < 0)
-            {
-                // Dataset does not exist
-
-                return null;
-            }
-
-            var dataspaceId = H5D.get_space(datasetId);
-
-            var rank = H5S.get_simple_extent_ndims(dataspaceId);
-
-
-            var dimensionSizes = new ulong[rank];
-
-            H5S.get_simple_extent_dims(dataspaceId, dimensionSizes, null);
-
-
-            //ulong[] dimensionSizes;
-
-            //if (rank == 0)
-            //{
-            //    //rank         =             1;
-            //    dimensionSizes = new[] { 1UL };
-            //}
-            //else
-            //{
-            //    dimensionSizes = new ulong[rank];
-
-            //    H5S.get_simple_extent_dims(dataspaceId, dimensionSizes, null);
-            //}
-
-
-
-            H5S.close(dataspaceId);
-
-
-
-            var dataArray = Array.CreateInstance(typeof(TValue), Array.ConvertAll(dimensionSizes, ds => (long)ds));
-
-            var arrayHandle = GCHandle.Alloc(dataArray, GCHandleType.Pinned);
-
-
-            var typeId = H5D.get_type(datasetId);
-
-
-            H5D.read(datasetId, typeId, H5S.ALL, H5S.ALL, H5P.DEFAULT, arrayHandle.AddrOfPinnedObject());
-
-            arrayHandle.Free();
-
-            H5T.close(typeId);
-            H5D.close(datasetId);
-
-            var info = new H5O.info_t();
-            H5O.get_info_by_name(FileIdentifier, datasetPath, ref info);
-
-            return new HdfData<TValue>(datasetPath, info.ctime == 0 ? null as ulong? : info.ctime, dataArray.Cast<TValue>().ToArray());
-        }
-
-        public string GetString(string datasetPath)
-        {
-            var datasetId = H5D.open(FileIdentifier, datasetPath);
-
-
-            if (datasetId < 0)
-            {
-                // Dataset does not exist
-
-                return null;
-            }
-
-            var typeId = H5D.get_type(datasetId);
-
-            Func<IntPtr, string> ptrToString;
-
-            var characterSet = H5T.get_cset(typeId);
-
-            switch (characterSet)
-            {
-                case H5T.cset_t.ASCII:
-                    ptrToString = Marshal.PtrToStringAnsi;
-
-                    break;
-                case H5T.cset_t.UTF8:
-                    ptrToString = Marshal.PtrToStringAuto;
-
-                    break;
-                default:
-                    throw new Exception($"Failed to parse string due to unsupported character set: {characterSet}");
-            }
-
-
-            var strPtrArray = new IntPtr[1];
-
-            var strHandle = GCHandle.Alloc(strPtrArray, GCHandleType.Pinned);
-            H5D.read(datasetId, typeId, H5S.ALL, H5S.ALL, H5P.DEFAULT, strHandle.AddrOfPinnedObject());
-
-            var strValue = ptrToString(strPtrArray[0]);
-
-            H5.free_memory(strPtrArray[0]);
-
-            strHandle.Free();
-
-            H5T.close(typeId);
-            H5D.close(datasetId);
-
-
-            return strValue;
-        }
-
-#elif NETSTANDARD2_1
         public HdfData<TValue> GetData<TValue>(string datasetPath)
             where TValue : unmanaged
         {
@@ -265,8 +148,6 @@
 
             return strValue;
         }
-#endif
-
 
 
         public void Dispose()
